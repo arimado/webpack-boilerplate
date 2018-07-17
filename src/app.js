e8b72f0d130b540e6cda74bd41b4d7511e100c8d
Jeremy Arimado <jeremyarimado@Jeremys-MacBook-Pro.local>
2018-07-17 21:33:09 +1000

    added react

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<h1 className="s">reactss</h1>, document.getElementById('react-root'));
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-require("babel-runtime/regenerator")
-require("webpack-hot-middleware/client?reload=true")
+require("babel-runtime/regenerator");
+require("babel-register");
+require("webpack-hot-middleware/client?reload=true");
 require("./main.css");
 require("./index.html");
-
-var a = async (args) => {
-    const { a, b } = args;
-    await console.log('nice', a, b);
-    console.log("done");
-}
-a({ a: 1, b: 4 });
+require("./app")
